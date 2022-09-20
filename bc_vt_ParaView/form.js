2a19ea1804b35ddf8bc4c64a06908f4b040c2372
ayush <ayush.6200@gmail.com>
2022-09-20 15:22:25 -0400

    updated form.js changes

--- bc_vt_ParaView/form.js
+++ bc_vt_ParaView/form.js
@@ -1,6 +1,32 @@
 'use strict'
+function register_listner() {
+  document.getElementById("batch_connect_session_context_cluster").addEventListener("change", change_event);
+}
+
+function change_event() {
+  var cluster_v = document.getElementById("batch_connect_session_context_cluster").value;
+  var cuda_ver = document.getElementById("batch_connect_session_context_bc_version").value;
+  var items;
+  if ( cluster_v == 'tinkercliffs')
+  {
+      items =  ["5.8.0-foss-2020a-Python-3.8.2-mpi","5.9.1-foss-2021a-mpi"];
+  }
+  else if ( cluster_v == 'infer')
+  {
+    items = ["5.8.0-foss-2020a-Python-3.8.2-mpi","5.8.1-foss-2020b-mpi"]
+  }
+ 
+  var str = ""
+  for (var item of items) {
+    str += "<option>" + item + "</option>"
+  }
+  
+  document.getElementById("batch_connect_session_context_bc_version").innerHTML = str;
+
+}
 
 $(document).ready(function() {
   // Set the max value to be what was set in the last session
-
-});
+ register_listner();
+ change_event();
+ });
